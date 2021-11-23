32c95f083efbbe344e162889314cddb3154be3ed
Braiskiskos <bmartinezparedes@danielcastelao.com>
2021-11-23 10:38:08 +0100

    404 + css con posicionamientos

--- html/wp-content/themes/newFuncy/404.php
+++ html/wp-content/themes/newFuncy/404.php
@@ -11,10 +11,46 @@ get_header();
 ?>
 
 <!--Mi 404 personalizado-->
-<h1 style="color:mediumslateblue">JAJAJAJAJA Lo que buscas no esta</h1>
+<h1 style="color:mediumslateblue" class="sticky">JAJAJAJAJA Lo que buscas no esta</h1>
     <div align="center"><img src="<?php echo get_stylesheet_directory_uri();?>/images/Escudo-Reconocimiento.png" width="480" height="270"></div>
+<div class="fixed"><img src="<?php echo get_stylesheet_directory_uri();?>/images/titan.jpg" width="256" height="160"><h2 class="absolute">PUES OK</h2></div>
 <h1 id="prueba">Los titanes han destruido lo que buscabas, hay que vengarse de ellos.<br>Doname 5€ y temeran nuestra colera</h1>
-
+<h1 class="relative">Matar Titanes</h1>
+<h1 class="relative">Matar Titanes</h1>
+<h1 class="relative">Matar Titanes</h1>
+<h1 class="relative">Matar Titanes</h1>
+<h1 class="relative">Matar Titanes</h1>
+<h1 class="relative">Matar Titanes</h1>
+<h1 class="relative">Matar Titanes</h1>
+<h1 class="relative">Matar Titanes</h1>
+<h1 class="relative">Matar Titanes</h1>
+<h1 class="relative">Matar Titanes</h1>
+<h1 class="relative">Matar Titanes</h1>
+<h1 class="relative">Matar Titanes</h1>
+<h1 class="relative">Matar Titanes</h1>
+<h1 class="relative">Matar Titanes</h1>
+<h1 class="relative">Matar Titanes</h1>
+<h1 class="relative">Matar Titanes</h1>
+<h1>Oremos para derrotarlos</h1>
+<h1 class="static">Pasta Nuestra,<br>
+    quien ‘Arrrr’ en el Cielo,<br>
+    Tragada sea tu vergüenza;<br>
+    Venga a nosotros Tu Retaco;<br>
+    Sea deliciosa Tu Salsa,<br>
+    Cúbrase de Parmesano rallado.<br>
+    <br>
+    Danos hoy nuestro pan de ajo.<br>
+    Y danos nuestros alfanjes,<br>
+    Así como nosotros bravuconeamos,<br>
+    nos echamos un trago y maldecimos.<br>
+    <br>
+    Y déjanos caer en la tentación,<br>
+    Pero envíanos una pizza.<br>
+    Pues tuyas son las albóndigas,<br>
+    la cerveza, y Strippers,<br>
+    por los siglos de los siglos.<br>
+    <br>
+    RAmen.</h1>
 <?php
 get_footer();
 ?>
